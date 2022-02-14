a759ce33a838531e33e839108a5916182bda5d8e
pavlos-mac <pavlos.macdonald-kosmidis@uk.clara.net>
2022-02-14 16:59:24 +0000

    * add hmac sha384

--- src/sha_hashes.rs
+++ src/sha_hashes.rs
@@ -1,8 +1,9 @@
 
 pub mod sha_hashes {
     use hmac::{Hmac, Mac};
-    use sha2::{Sha224, Sha256, Digest};
+    use sha2::{Sha224, Sha256, Digest, Sha384};
     use sha3::{Sha3_384};
+    use generic_array;
 
     pub fn _sha_224(input: &str) -> String {
         let mut hasher = Sha224::new();
@@ -33,6 +34,17 @@ pub mod sha_hashes {
         let h = hex::encode(&as_bytes);
         h
     }
+
+    pub fn _hmac_sha384(msg: String, key: String) -> String {
+        type HmacSha384 = Hmac<Sha384>;
+
+        let mut hmac = HmacSha384::new_from_slice(key.as_bytes()).expect("it to work");
+        hmac.update(msg.as_bytes());
+
+        let r = hmac.finalize();
+        let result_arr = r.into_bytes();
+        format!("{:x}", result_arr)
+    }
 }
 
 #[cfg(test)]
@@ -40,6 +52,7 @@ mod tests {
     use crate::sha_hashes::sha_hashes::_sha_224;
     use crate::sha_hashes::sha_hashes::_sha_384;
     use crate::sha_hashes::sha_hashes::_hmac_sha256;
+    use crate::sha_hashes::sha_hashes::_hmac_sha384;
 
     #[test]
     fn test_sha_224() {
@@ -58,7 +71,16 @@ mod tests {
     #[test]
     fn test_hmac_sha256() {
         let str = _hmac_sha256(String::from("sample message"), String::from("12345"));
-
         assert_eq!(str, "ee40ca7bc90df844d2f5b5667b27361a2350fad99352d8a6ce061c69e41e5d32");
+    }
+
+    #[test]
+    fn test_hmac_sha384() {
+        // https://cryptobook.nakov.com/mac-and-key-derivation/exercises-calculate-hmac
+        let get_hmac1 = _hmac_sha384(String::from("hello"), String::from("cryptography"));
+        assert_eq!(get_hmac1, "83d1c3d3774d8a32b8ea0460330c16d1b2e3e5c0ea86ccc2d70e603aa8c8151d675dfe339d83f3f495fab226795789d4");
+
+        let get_hmac2 = _hmac_sha384(String::from("hello"), String::from("again"));
+        assert_eq!(get_hmac2, "4c549a549aa037e0fb651569bf271faa23cfa20e8a9d21438a6ff5bf6be916bebdbaa48001e0cd6941ec74cd02be70e5");
     }
 }
